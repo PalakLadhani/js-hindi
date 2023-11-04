73b802d17f3eea2fd783d45515505bdd5e5f8558
PalakLadhani <121243142+PalakLadhani@users.noreply.github.com>
2023-11-04 16:11:01 +0000

    datatype summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+// Primitive
+
+// 7 types : String,Number,Boolean,null,undefined,symbol.BigInt
+
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId =Symbol('123')
+
+console.log(id===anotherId);
+
+
+//const bigNumber=3456566668698896966n
+
+
+//Reference (non-primitive)
+
+//Array,Objects , Function
+
+
+const heros=["shaktiman","naagraj","doga"]
+let myObj={
+    name:"Palak",
+    age:21,
+}
+
+
+const myFunction=function(){
+    console.log("heloo world");
+
+}
+
+console.log(typeof anotherId);
